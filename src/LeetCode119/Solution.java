6852edcdc10ee740cc0a4bff170ce77186be0eb5
fengxiangli <fengxiangli@6tiantian.com>
2016-01-31 14:14:58 +0800

    LeetCode119

---
+++ src/LeetCode119/Solution.java
@@ -0,0 +1,41 @@
+package LeetCode119;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution {
+	public static List<Integer> getRow(int rowIndex) {
+		List<Integer> lastRow = new ArrayList<Integer>();
+		List<Integer> currentRow = new ArrayList<Integer>();
+		int numTempRow = 0;
+		while (numTempRow < rowIndex+1) {
+			if (numTempRow == 0) {
+				lastRow.clear();
+				lastRow.add(1);
+				currentRow.clear();
+				currentRow.addAll(lastRow);
+			}else if (numTempRow == 1) {
+				currentRow.clear();
+				currentRow.add(1);
+				currentRow.add(1);
+				lastRow.clear();
+				lastRow.addAll(currentRow);
+			}else {
+				currentRow.clear();
+				currentRow.add(1);
+				for (int i = 0; i < lastRow.size() - 1; i++) {
+					currentRow.add(lastRow.get(i)+lastRow.get(i+1));
+				}
+				currentRow.add(1);
+				lastRow.clear();
+				lastRow.addAll(currentRow);
+			}
+			numTempRow ++;
+		}
+		return currentRow;
+	}
+	
+	public static void main(String[] args) {
+		System.out.println(getRow(1));
+	}
+}
